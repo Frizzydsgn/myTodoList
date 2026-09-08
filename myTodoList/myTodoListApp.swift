924f8e1b1cbdd37457b533be2f4d2f3592d99130
Freeborn <freezyisdope@gmail.com>
2026-09-08 16:20:27 +0100

    Initial Commit

---
+++ myTodoList/myTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTodoListApp.swift
+//  myTodoList
+//
+//  Created by Freeborn Ehirhere on 08/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct myTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
